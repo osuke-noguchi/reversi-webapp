8db3f2f3a924e2b6fc9ad801da9bb1253553a26d
osuke-noguchi <noguchi.nodai@gmail.com>
2024-03-02 05:32:43 +0900

    rename infrastructure from dataaccess

--- src/domain/turn/turnRepository.ts
+++ src/domain/turn/turnRepository.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
-import { MoveGateway } from '../../dataaccess/moveGateway';
-import { SquareGateway } from '../../dataaccess/squareGateway';
-import { TurnGateway } from '../../dataaccess/turnGateway';
+import { MoveGateway } from '../../infrastructure/moveGateway';
+import { SquareGateway } from '../../infrastructure/squareGateway';
+import { TurnGateway } from '../../infrastructure/turnGateway';
 import { Board } from './board';
 import { toDisc } from './disc';
 import { Move } from './move';
